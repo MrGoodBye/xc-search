4eba8387c7299e81ce32aaf951f0662ace30cc49
MrGoodBye <1254993267@qq.com>
2020-08-31 12:12:33 +0800

    webpack dev and prod configurations

--- scripts/config/webpack.common.js
+++ scripts/config/webpack.common.js
@@ -1,11 +1,34 @@
 const path = require('path')
+const { PROJECT_PATH, isDev } = require('../consts')
 
 module.exports = {
   entry: {
-    app: path.resolve(__dirname, '../../src/app.js'),
+    app: path.resolve(PROJECT_PATH, './src/app.js'),
   },
   output: {
-    filename: 'js/[name].[hash:8].js',
-    path: path.resolve(__dirname, '../../dist'),
+    filename: `js/[name]${isDev ? '' : '.[hash:8]'}.js`,
+    path: path.resolve(PROJECT_PATH, './dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(PROJECT_PATH, './public/index.html'),
+      filename: 'index.html',
+      minify: isDev
+        ? false
+        : {
+            removeAttributeQuotes: true,
+            collapseWhitespace: true,
+            removeComments: true,
+            collapseBooleanAttributes: true,
+            collapseInlineTagWhitespace: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            minifyCSS: true,
+            minifyJS: true,
+            minifyURLs: true,
+            useShortDoctype: true,
+          },
+    }),
+  ],
 }
---
+++ scripts/config/webpack.dev.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+const { SERVER_HOST, SERVER_PORT } = require('../consts')
+
+module.exports = merge(common, {
+  mode: 'development',
+  devServer: {
+    host: SERVER_HOST,
+    port: SERVER_PORT,
+    stats: 'errors-only',
+    clientLogLevel: 'silent',
+    compress: true,
+    open: true,
+    hot: true,
+  },
+})
