dc6a3ba53d1f7101491b316bf354bdf52a4dd5d3
MrGoodBye <1254993267@qq.com>
2020-09-01 07:18:02 +0800

    add babel configrations

--- scripts/config/webpack.common.js
+++ scripts/config/webpack.common.js
@@ -1,17 +1,28 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { PROJECT_PATH, isDev } = require('../consts')
 
 module.exports = {
   entry: {
-    app: path.resolve(PROJECT_PATH, './src/app.js'),
+    app: path.resolve(PROJECT_PATH, './src/index.js'),
   },
   output: {
     filename: `js/[name]${isDev ? '' : '.[hash:8]'}.js`,
     path: path.resolve(PROJECT_PATH, './dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?|js)$/,
+        loader: 'babel-loader',
+        options: { cacheDirectory: true },
+        exclude: /node_modules/,
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: resolve(PROJECT_PATH, './public/index.html'),
+      template: path.resolve(PROJECT_PATH, './public/index.html'),
       filename: 'index.html',
       minify: isDev
         ? false
