eb8bb17b619c13887e2d3a5d13dca93a78bc39ad
MrGoodBye <1254993267@qq.com>
2020-09-01 08:45:31 +0800

    lint with prettier conflict extension

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,10 @@ module.exports = {
     'plugin:unicorn/recommended',
     'plugin:promise/recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'prettier/unicorn',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
